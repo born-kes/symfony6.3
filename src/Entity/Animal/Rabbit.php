8ea65468f2787ab8cb1ede808eb0ecab200a875a
born-kes <born-kes@o2.pl>
2024-02-29 21:25:26 +0100

    solving the task Zoo

---
+++ src/Entity/Animal/Rabbit.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity\Animal;
+
+use App\Entity\Animal\Characteristic\IFur;
+use App\Entity\Food\FoodType;
+
+class Rabbit extends Animal implements IFur
+{
+    use Characteristic\Fur;
+
+    protected string $species = 'Królik';
+
+    protected function getElementDiet(): array
+    {
+        return [FoodType::VEGETABLES];
+    }
+
+    public function groom(): void
+    {
+        $this->setCleanFur();
+    }
+
+    public function presentsItself(): string
+    {
+        return sprintf($this->hasFur() ? self::FUR_CLEAN : self::FUR_DIRTY, $this);
+    }
+}
---
+++ src/Entity/Animal/Tiger.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity\Animal;
+
+use App\Entity\Animal\Characteristic\IFur;
+use App\Entity\Food\FoodType;
+
+class Tiger extends Animal implements IFur
+{
+    use Characteristic\Fur;
+
+    protected string $species = 'Tygrys';
+
+    protected function getElementDiet(): array
+    {
+        return [FoodType::MEAT];
+    }
+
+    public function groom(): void
+    {
+        $this->setCleanFur();
+    }
+
+    public function presentsItself(): string
+    {
+        return sprintf($this->hasFur() ? self::FUR_CLEAN : self::FUR_DIRTY, $this);
+    }
+}
