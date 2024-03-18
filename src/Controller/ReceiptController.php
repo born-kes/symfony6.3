90b78a4d62e523b7fb40e48b2d0d33b11a3f5c2c
born-kes <born-kes@o2.pl>
2024-03-18 08:03:19 +0100

    refactor

--- src/Controller/ReceiptController.php
+++ src/Controller/ReceiptController.php
@@ -4,16 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Receipt;
 use App\Form\ReceiptType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 class ReceiptController extends AbstractController
 {
     #[Route('/receipt', name: 'app_receipt')]
-    public function index(Request $request,  PersistenceManagerRegistry $doctrine): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $receipt = new Receipt();
         $form = $this->createForm(ReceiptType::class, $receipt);
@@ -21,14 +21,13 @@ class ReceiptController extends AbstractController
 
         $productName = '';
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $productName = $receipt->getProduct()->getName();
             $productName .= ' ' . $receipt->getPrice();
 
             $entityManager = $doctrine->getManager();
             $entityManager->persist($receipt);
             $entityManager->flush();
-            ;
         }
 
 
